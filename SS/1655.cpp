4d0979c7de107159c5c27e9ec6ba70321605d3b2
HyeJuSeon <55024771+HyeJuSeon@users.noreply.github.com>
2022-01-05 23:39:22 +0900

    Add files via upload

--- SS/1655.cpp
+++ SS/1655.cpp
@@ -1,37 +1,26 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-priority_queue<int> Data;
-int get_mid() {
-    priority_queue<int> pq;   
-    int len = Data.size(); 
-    for (int i = 0; i < len / 2; i++) {
-        pq.push(Data.top());
-        Data.pop();
-    }
-    int res = Data.top();
-    while (!pq.empty()) {
-        Data.push(pq.top());
-        pq.pop();
-    }
-    return res;
-}
-
 int main() {
     ios_base::sync_with_stdio(0);
     cin.tie(0);
     
+    priority_queue<int> maxheap, minheap;
     int n;
     cin >> n;
     while (n--) {
         int num;
         cin >> num;
-        Data.push(num);
-        if (Data.size() < 2) {
-            cout << Data.top() << '\n';
-            continue;
+        if (maxheap.size() > minheap.size()) minheap.push(-num);
+        else maxheap.push(num);
+        if (maxheap.size() && minheap.size() && maxheap.top() > -minheap.top()) {
+            int tmp = maxheap.top();
+            maxheap.pop();
+            maxheap.push(-minheap.top());
+            minheap.pop();
+            minheap.push(-tmp);
         }
-        cout << get_mid() << '\n';
+        cout << maxheap.top() << '\n';
     }
     return 0;
 }
