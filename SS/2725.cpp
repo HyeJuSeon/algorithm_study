b2f1405599821412ac88112931d449802297585d
HyeJuSeon <55024771+HyeJuSeon@users.noreply.github.com>
2022-01-07 00:59:17 +0900

    Add files via upload

--- SS/2725.cpp
+++ SS/2725.cpp
@@ -17,6 +17,9 @@ int eulerPhi(int n) {
 }
 
 int main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    
     for (int i = 2; i <= MAXN; i++) {
         if (Visit[i]) continue;
         for (int j = i * i; j <= MAXN; j += i) Visit[j] = true;
