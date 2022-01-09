24aa2652d2d23030f50d931fbdfd5a8343c27d3e
HyeJuSeon <55024771+HyeJuSeon@users.noreply.github.com>
2022-01-09 22:57:11 +0900

    Add files via upload

--- SS/11050.cpp
+++ SS/11050.cpp
@@ -1,18 +1,19 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int factorial(int n) {
-    int res = 1;
-    for (int i = 1; i <= n; i++) res *= i;
-    return res;
-}
-
+#define MAXN 10
+int DP[MAXN + 1][MAXN + 1];
 int main() {
     ios_base::sync_with_stdio(0);
     cin.tie(0);
     
     int n, k;
     cin >> n >> k;
-    cout << factorial(n) / (factorial(k) * factorial(n - k));
+    DP[0][0] = 1;
+    for (int i = 1; i <= n; i++) {
+        DP[i][0] = 1;
+        for (int j = 1; j <= i && j <= k; j++) DP[i][j] = DP[i - 1][j] + DP[i - 1][j - 1];
+    }
+    cout << DP[n][k];
     return 0;
 }
