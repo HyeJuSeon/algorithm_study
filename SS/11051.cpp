4ba1a3406f03b720daaf2e9a525be6f2b1c1148b
HyeJuSeon <55024771+HyeJuSeon@users.noreply.github.com>
2022-01-09 23:02:18 +0900

    Add files via upload

---
+++ SS/11051.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define MAXN 1000
+#define MOD 10007
+int DP[MAXN + 1][MAXN + 1];
+int main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    
+    int n, k;
+    cin >> n >> k;
+    DP[0][0] = 1;
+    for (int i = 1; i <= n; i++) {
+        DP[i][0] = 1;
+        for (int j = 1; j <= i && j <= k; j++) DP[i][j] = (DP[i - 1][j] + DP[i - 1][j - 1]) % MOD;
+    }
+    cout << DP[n][k] << '\n';
+    return 0;
+}
