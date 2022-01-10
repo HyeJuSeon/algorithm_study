3959c58e3f56348f9cb0d9f9847cbc1eb878fd7d
HyeJuSeon <55024771+HyeJuSeon@users.noreply.github.com>
2022-01-10 11:13:25 +0900

    Add files via upload

---
+++ SS/1717.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+using namespace std;
+
+#define MAXN 1000000
+#define MAXM 100000
+int Parent[MAXN];
+int find(int a) {
+    if (Parent[a] == a) return a;
+    return Parent[a] = find(Parent[a]);
+}
+
+void uni(int a, int b) {
+    Parent[find(a)] = find(b);
+}
+
+int main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+
+    int n, m;
+    cin >> n >> m;
+    for (int i = 0; i <= n; i++) Parent[i] = i;
+    while (m--) {
+        int op, a, b;
+        cin >> op >> a >> b;
+        if (op == 0) {
+            uni(a, b);
+        }
+        else {
+            if (find(a) == find(b)) cout << "YES" << '\n';
+            else cout << "NO" << '\n';
+        }
+    }
+    return 0;
+}
