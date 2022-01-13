c4f7e872ef0e64341f58705be5289a1500bc03e2
HyeJuSeon <55024771+HyeJuSeon@users.noreply.github.com>
2022-01-13 13:10:26 +0900

    Add files via upload

--- SS/11660.cpp
+++ SS/11660.cpp
@@ -3,6 +3,7 @@
 using namespace std;
 
 int DP[MAXN + 1][MAXN + 1];
+int Mat[MAXN + 1][MAXN + 1];
 int N, M;
 int main() {
     ios_base::sync_with_stdio(0);
@@ -11,18 +12,14 @@ int main() {
     cin >> N >> M;
     for (int i = 1; i <= N; i++) {
         for (int j = 1; j <= N; j++) {
-            cin >> DP[i][j];
-            DP[i][j] += DP[i][j - 1];
+            cin >> Mat[i][j];
+            DP[i][j] = DP[i][j - 1] + DP[i - 1][j] - DP[i - 1][j - 1] + Mat[i][j];
         }
     }
     int x1, x2, y1, y2;
     while (M--) {
-        int res = 0;
         cin >> x1 >> y1 >> x2 >> y2;
-        for (int i = x1; i <= x2; i++) {
-            res += DP[i][y2] - DP[i][y1 - 1];
-        }
-        cout << res << '\n';
+        cout << DP[x2][y2] - DP[x2][y1 - 1]- DP[x1 - 1][y2] + DP[x1 - 1][y1 - 1] << '\n';
     }
     return 0;
 }
