f8764a901f0b97988d7ec4f3f5b904af54ffc735
HyeJuSeon <55024771+HyeJuSeon@users.noreply.github.com>
2022-01-13 13:27:06 +0900

    Add files via upload

--- SS/11660.cpp
+++ SS/11660.cpp
@@ -3,7 +3,7 @@
 using namespace std;
 
 int DP[MAXN + 1][MAXN + 1];
-int Mat[MAXN + 1][MAXN + 1];
+int Map[MAXN + 1][MAXN + 1];
 int N, M;
 int main() {
     ios_base::sync_with_stdio(0);
@@ -12,8 +12,8 @@ int main() {
     cin >> N >> M;
     for (int i = 1; i <= N; i++) {
         for (int j = 1; j <= N; j++) {
-            cin >> Mat[i][j];
-            DP[i][j] = DP[i][j - 1] + DP[i - 1][j] - DP[i - 1][j - 1] + Mat[i][j];
+            cin >> Map[i][j];
+            DP[i][j] = DP[i][j - 1] + DP[i - 1][j] - DP[i - 1][j - 1] + Map[i][j];
         }
     }
     int x1, x2, y1, y2;
