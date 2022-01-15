bc7307a82cbef66deaf02fcb30de0edf90d0cf36
HyeJuSeon <55024771+HyeJuSeon@users.noreply.github.com>
2022-01-15 22:18:31 +0900

    Add files via upload

---
+++ SS/11049.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#define MAXN 500
+#define INF 1000000000
+using namespace std;
+
+int DP[MAXN][MAXN];
+int A[MAXN][2]; 
+int N;
+int dfs (int x, int y) {
+    if (x == y) return 0;
+    if (DP[x][y] != INF) return DP[x][y];
+    for (int i = x; i < y; i++) 
+        DP[x][y] = min(DP[x][y], dfs(x, i) + dfs(i + 1, y) + A[x][0] * A[i][1] * A[y][1]);
+    return DP[x][y];
+}
+
+int main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+
+    cin >> N;
+    for (int i = 0; i < N; i++) {
+        cin >> A[i][0] >> A[i][1];
+        for (int j = 0; j < N; j++) DP[i][j] = INF;
+    }
+    cout << dfs(0, N - 1);
+    return 0;
+}
