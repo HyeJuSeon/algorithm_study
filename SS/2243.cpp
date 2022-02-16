bc51cf2acdead6dc0129889bdbd38f2c8be9a11a
HyeJuSeon <55024771+HyeJuSeon@users.noreply.github.com>
2022-02-17 00:55:24 +0900

    Update 2243.cpp

--- SS/2243.cpp
+++ SS/2243.cpp
@@ -1,40 +1,42 @@
 #include <iostream>
+#define MAXN 3000000
 #define MAXC 1000000
 using namespace std;
 
-int IDT[MAXC * 3];
-int N, Start = 1;
-void update(int i, int num) {
-    i += Start - 1;
+int IDT[MAXN];
+int N, S = 1;
+void update(int i, int value) {
+    i += S - 1;
     while (i > 0) {
-        IDT[i] += num;
+        IDT[i] += value;
         i /= 2;
     }
 }
 
-int get(int num) {
+int out(int rank) {
     int i = 1;
-    while (i < Start) {
+    IDT[i]--;
+    while (i < S) {
         i *= 2;
-        if (num > IDT[i]) {
-            num -= IDT[i];
+        if (rank > IDT[i]) {
+            rank -= IDT[i];
             i++;
         }
         IDT[i]--;
     }
-    return i - Start + 1;
+    return i - S + 1;
 }
 
 int main() {
     ios_base::sync_with_stdio(0);
     cin.tie(0);
 
+    while (S < MAXC) S *= 2;
     cin >> N;
-    while (Start < MAXC) Start *= 2;
     int a, b, c;
     while (N--) {
         cin >> a >> b;
-        if (a == 1) cout << get(b) << '\n';
+        if (a == 1) cout << out(b) << '\n';
         else {
             cin >> c;
             update(b, c);
