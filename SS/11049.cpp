2a5d0518b58b2792832a3a22b361c7a2f490086d
HyeJuSeon <55024771+HyeJuSeon@users.noreply.github.com>
2022-02-18 13:24:24 +0900

    Update 11049.cpp

--- SS/11049.cpp
+++ SS/11049.cpp
@@ -1,28 +1,26 @@
 #include <iostream>
-#define MAXN 500
-#define INF 1000000000
+#define MAXN 501
+#define INF 2000000000
 using namespace std;
 
-int DP[MAXN][MAXN]; // DP[x][y]: x ~ y까지 곱했을 떄 최소값
-int A[MAXN][2]; 
+int M[MAXN][2];
+int DP[MAXN][MAXN]; // x~y번 행렬의 최소 연산 횟수
 int N;
-int dfs (int x, int y) {
-    if (x == y) return 0;
-    if (DP[x][y] != INF) return DP[x][y];
-    for (int i = x; i < y; i++) 
-        DP[x][y] = min(DP[x][y], dfs(x, i) + dfs(i + 1, y) + A[x][0] * A[i][1] * A[y][1]);
-    return DP[x][y];
-}
-
 int main() {
     ios_base::sync_with_stdio(0);
     cin.tie(0);
 
     cin >> N;
-    for (int i = 0; i < N; i++) {
-        cin >> A[i][0] >> A[i][1];
-        for (int j = 0; j < N; j++) DP[i][j] = INF;
+    for (int i = 0; i < N; i++) cin >> M[i][0] >> M[i][1];
+    for (int m = 1; m < N; m++) {
+        for (int i = 0; i < N - m; i++) {
+            int j = m + i;
+            DP[i][j] = INF;
+            for (int k = i; k < j; k++) {
+                DP[i][j] = min(DP[i][j], DP[i][k] + DP[k + 1][j] + M[i][0] * M[k][1] * M[j][1]);
+            }
+        }
     }
-    cout << dfs(0, N - 1);
+    cout << DP[0][N - 1];
     return 0;
 }
