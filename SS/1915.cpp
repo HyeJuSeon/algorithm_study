642a7f33cd0d66846fdfb55251dbf50a859f7aae
HyeJuSeon <55024771+HyeJuSeon@users.noreply.github.com>
2022-02-18 12:00:51 +0900

    Update 1915.cpp

--- SS/1915.cpp
+++ SS/1915.cpp
@@ -1,26 +1,25 @@
 #include <iostream>
-#define MAXN 1000
+#define MAXN 1001
 using namespace std;
 
-int DP[MAXN + 1][MAXN + 1];
-char Input[MAXN + 1];
-int N, M;
+int DP[MAXN][MAXN];
+char Input[MAXN];
+int N, M, Ans = 0;
 int main() {
     ios_base::sync_with_stdio(0);
     cin.tie(0);
 
     cin >> N >> M;
-    int res = 0;
     for (int i = 1; i <= N; i++) {
         cin >> Input;
         for (int j = 1; j <= M; j++) {
             DP[i][j] = Input[j - 1] - '0';
             if (DP[i][j]) {
-                DP[i][j] = min(DP[i - 1][j], min(DP[i][j - 1], DP[i - 1][j - 1])) + 1;
-                res = max(res, DP[i][j]);
+                DP[i][j] += min(DP[i - 1][j - 1], min(DP[i][j - 1], DP[i - 1][j]));
+                Ans = max(Ans, DP[i][j]);
             }
         }
     }
-    cout << res * res << '\n';
+    cout << Ans * Ans;
     return 0;
 }
