d04a256deac6aecfee4986fe2059c9fdcce95a5a
HyeJuSeon <55024771+HyeJuSeon@users.noreply.github.com>
2022-02-18 11:17:31 +0900

    Update 2579.cpp

--- SS/2579.cpp
+++ SS/2579.cpp
@@ -2,9 +2,9 @@
 #define MAXN 300
 using namespace std;
 
-int DP[MAXN];
-int S[MAXN];
 int N;
+int S[MAXN];
+int DP[MAXN];
 int main() {
     ios_base::sync_with_stdio(0);
     cin.tie(0);
@@ -13,8 +13,8 @@ int main() {
     for (int i = 0; i < N; i++) cin >> S[i];
     DP[0] = S[0];
     DP[1] = S[0] + S[1];
-    DP[2] = max(S[0] + S[2], S[1] + S[2]);
-    for (int i = 3; i < N; i++) DP[i] = max(DP[i - 2], S[i - 1] + DP[i - 3]) + S[i];
-    cout << DP[N - 1] << '\n';
+    DP[2] = max(S[0], S[1]) + S[2];
+    for (int i = 3; i < N; i++) DP[i] = max(S[i - 1] + DP[i - 3], DP[i - 2]) + S[i];
+    cout << DP[N - 1];
     return 0;
 }
