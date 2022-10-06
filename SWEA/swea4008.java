e68727108e9654476aa21b5127cb0bc53b35abd2
HyejuSeon <55024771+HyejuSeon@users.noreply.github.com>
2022-10-06 19:51:58 +0900

    4008. 숫자 만들기

--- SWEA/swea4008.java
+++ SWEA/swea4008.java
@@ -8,8 +8,8 @@ import java.util.StringTokenizer;
 // 숫자 만들기 
 public class swea4008 {
 	static int N, min, max;
-	static int[] ops, ops_, nums;
-	static boolean[] visited;
+	static int[] ops = new int[4]; // + - * /
+	static int[] nums, permu;
 	
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -20,20 +20,17 @@ public class swea4008 {
 			min = Integer.MAX_VALUE;
 			max = -Integer.MAX_VALUE;
 			N = Integer.parseInt(br.readLine());
-			ops = new int[N - 1]; // + - * /
+			
 			nums = new int[N];
+			permu = new int[N - 1];
 			stk = new StringTokenizer(br.readLine(), " ");
-			int idx = 0;
 			for (int i = 0; i < 4; i++) {
-				int tmp = Integer.parseInt(stk.nextToken());
-				for (int j = 0; j < tmp; j++)
-					ops[idx++] = i;
+				ops[i] = Integer.parseInt(stk.nextToken());
 			}
 			stk = new StringTokenizer(br.readLine(), " ");
-			for (int i = 0; i < N; i++) 
+			for (int i = 0; i < N; i++) {
 				nums[i] = Integer.parseInt(stk.nextToken());
-			visited = new boolean[N - 1];
-			ops_ = new int[N - 1];
+			}
 			dfs(0);
 			sb.append("#").append(t).append(" ").append(max - min).append("\n");
 		}
@@ -43,12 +40,12 @@ public class swea4008 {
 	private static void dfs(int depth) {
 		if (depth == N - 1) {
 			int res = nums[0];
-			for (int i = 0; i < ops_.length; i++) {
-				if (ops_[i] == 0) 
+			for (int i = 0; i < permu.length; i++) {
+				if (permu[i] == 0) 
 					res += nums[i + 1];
-				else if (ops_[i] == 1)
+				else if (permu[i] == 1)
 					res -= nums[i + 1];
-				else if (ops_[i] == 2)
+				else if (permu[i] == 2)
 					res *= nums[i + 1];
 				else
 					res /= nums[i + 1];
@@ -57,12 +54,13 @@ public class swea4008 {
 			max = res > max ? res : max;
 			return;
 		}
-		for (int i = 0; i < N - 1; i++) {
-			if (visited[i]) continue;
-			ops_[depth] = ops[i];
-			visited[i] = true;
-			dfs(depth + 1);
-			visited[i] = false;
+		for (int i = 0; i < 4; i++) {
+			if (ops[i] > 0) {
+				ops[i]--;
+				permu[depth] = i;
+				dfs(depth + 1);
+				ops[i]++;
+			}
 		}
 	}
 }
